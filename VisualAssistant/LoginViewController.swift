6d9ed823be7dedb90b953dc3ec2c6c0c3658196a
jianyanglee <leesherlock13@gmail.com>
2019-10-27 14:17:40 +1100

    log in and register pages done!

--- VisualAssistant/LoginViewController.swift
+++ VisualAssistant/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
@@ -14,7 +15,11 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var password: UITextField!
     
-    @IBOutlet weak var statusLabel: UIImageView!
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    @IBOutlet weak var button: UIButton!
+    
+    var control: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +27,42 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func btnLogin(_ sender: Any) {
+         let email = username.text!
+         let passwd = password.text!
+                Auth.auth().signIn(withEmail: email, password: passwd){
+                    [weak self] user, error in
+                    guard let strongSelf = self else { return }
+                    if (error != nil){
+                        strongSelf.statusLabel.text = "Error happens, Check your user email and password"
+                        self!.username.text = ""
+                        self!.password.text = ""
+                        print(error)
+                        return
+                    }
+                    self!.button.setTitle("Confirm", for: .normal)
+                    strongSelf.statusLabel.text = "Verification done! Confirm?"
+                    self!.control = true
+        //            self!.performSegue(withIdentifier: "loginSegue", sender: nil)
+                }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "loginSegue" {
+            let destination = segue.destination as! HomeControllerViewController
+        }
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+          if identifier == "loginSegue"{
+              if control {
+                  return true
+              }
+              return false
+          }
+          return true
+      }
+    
     /*
     // MARK: - Navigation
 
